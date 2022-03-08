50e01294d828ece497daa1a217e1ad37470d70aa
Enzo Falone <enzofalone123@gmail.com>
2022-03-07 23:48:05 -0500

    modal styling

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 const boardDiv = document.querySelector(".game-board");
 const headerDiv = document.querySelector(".header-wrapper");
 
-function newGame() {
-
-}
-
 function restart() {
     gameBoard.resetGame();
     openModal(document.querySelector('#modal-new-game'))
